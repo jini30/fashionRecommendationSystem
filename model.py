92326b873bff00145872ac08f08529425865aaec
Megha Prajapati <119098434+jini30@users.noreply.github.com>
2024-07-13 22:31:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 m = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 m.trainable = False
 
-m.save('model.keras')
+m.save('resnet50_model.h5')
